efc186c0bca8cfb6564ae848767a1f7814fcec03
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-02 22:15:56 +0545

    FAQs Completed

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PricingController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\FAQController;
 
 Route::post('/contact', [ContactController::class, 'contact']);
 
@@ -15,6 +16,8 @@ Route::get('/blogs', [BlogController::class, 'blogs']);
 Route::get('/case-studies', [BlogController::class, 'caseStudies']);
 
 Route::get('/blog/{slug}', [BlogController::class, 'show']);
+
+Route::get('/faqs', [FAQController::class, 'getFaq']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
