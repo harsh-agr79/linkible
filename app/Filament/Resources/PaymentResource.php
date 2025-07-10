bfb213e5a08ff0c80d903ff6ba92b1e0354bb402
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-11 00:03:34 +0530

    changes

---
+++ app/Filament/Resources/PaymentResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentResource\Pages;
+use App\Filament\Resources\PaymentResource\RelationManagers;
+use App\Models\Payment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TextareaColumn;
+
+class PaymentResource extends Resource
+{
+    protected static ?string $model = Payment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('payment_intent_id')
+                    ->label('Intent ID')
+                    ->copyable()
+                    ->searchable()
+                    ->wrap(),
+
+                BadgeColumn::make('status')
+                    ->colors([
+                        'success' => 'succeeded',
+                        'danger' => 'failed',
+                        'warning' => ['processing', 'requires_payment_method'],
+                        'gray' => 'incomplete',
+                    ])
+                    ->sortable(),
+
+                TextColumn::make('amount')
+                    ->label('Amount ($)')
+                    ->money('usd', divideBy: 100)
+                    ->sortable(),
+
+                TextColumn::make('currency')->uppercase(),
+
+                TextColumn::make('email')
+                    ->label('Customer Email')
+                    ->searchable()
+                    ->wrap(),
+
+                TextColumn::make('created_at')
+                    ->label('Date')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+     public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPayments::route('/'),
+            // 'create' => Pages\CreatePayment::route('/create'),
+            'edit' => Pages\EditPayment::route('/{record}/edit'),
+        ];
+    }
+}
