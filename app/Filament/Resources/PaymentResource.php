15f85cdbddd9132f969146bfc74ce1569d87232c
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-11 00:06:00 +0530

    changes

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -70,11 +70,11 @@ class PaymentResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
