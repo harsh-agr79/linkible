8a0d03756d878b76e4c5aa38afad7d08c23471b0
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-12 15:41:08 +0530

    contact info resource and api

---
+++ app/Filament/Resources/ContactInfoResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactInfoResource\Pages;
+use App\Filament\Resources\ContactInfoResource\RelationManagers;
+use App\Models\ContactInfo;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+
+class ContactInfoResource extends Resource
+{
+    protected static ?string $model = ContactInfo::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make('icon')
+                    ->image()
+                    ->directory('contact-icons')
+                    ->required(),
+                TextInput::make('title')->required(),
+                TextInput::make('value')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('icon')->circular(),
+                TextColumn::make('title'),
+                TextColumn::make('value'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactInfos::route('/'),
+            'create' => Pages\CreateContactInfo::route('/create'),
+            'edit' => Pages\EditContactInfo::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\About;
+use App\Models\ContactInfo;
 
 class AboutController extends Controller
 {
     public function getAboutUs(Request $request){
         $about = About::first();
+        $contact = ContactInfo::all();
+
+        // Attach as a new attribute
+        $about->contact_info = $contact;
+
         return response()->json($about, 200);
     }
 }
